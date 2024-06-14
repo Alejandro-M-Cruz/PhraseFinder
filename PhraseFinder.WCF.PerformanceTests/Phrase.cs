8f66d1574124578fa3b99813bdc9af50a6383da4
Alejandro-M-Cruz <alejandromatiascc@gmail.com>
2024-06-14 12:11:07 +0100

    Removed unused files

--- PhraseFinder.WCF.PerformanceTests/Phrase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PhraseFinder.WCF.PerformanceTests;
-
-internal class Phrase
-{
-    public int PhraseId { get; set; }
-    public string Value { get; set; }
-    public string Pattern { get; set; }
-    public string BaseWord { get; set; }
-}
